2ba5d6ecbf27a2e85a2cbeab565cb45bc1bf9bf5
Waylon Dixon <wdixon@waylondixon.com>
2017-09-09 05:00:21 +0000

    Layouts & Structures: convert from album to blog template

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,41 +8,33 @@
     <meta name="author" content="">
     <link rel="icon" href="../../../../favicon.ico">
 
-    <title>Album example for Bootstrap</title>
+    <title>Blog Template for Bootstrap</title>
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 
     <!-- Custom styles for this template -->
-    <link href="/css/album.css" rel="stylesheet">
+    <link href="/css/blog.css" rel="stylesheet">
   </head>
 
   <body>
 
-    <div class="collapse bg-dark" id="navbarHeader">
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-8 py-4">
-            <h4 class="text-white">About</h4>
-            <p class="text-muted">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
-          </div>
-          <div class="col-sm-4 py-4">
-            <h4 class="text-white">Contact</h4>
-            <ul class="list-unstyled">
-              <li><a href="#" class="text-white">Follow on Twitter</a></li>
-              <li><a href="#" class="text-white">Like on Facebook</a></li>
-              <li><a href="#" class="text-white">Email me</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-
     @include('layouts/nav')
 
+    @include('layouts/header')
+
     <div class="container">
-      @yield('content')
-    </div>
+      <div class="row">
+        <div class="col-sm-8 blog-main">
+          @yield('content')
+        </div><!-- /.blog-main -->
+        
+        <div class="col-sm-3 offset-sm-1 blog-sidebar">
+          @include('layouts/sidebar')
+        </div><!-- /.blog-sidebar -->
+
+      </div><!-- /.row -->
+    </div><!-- /.container -->
 
     @include('layouts/footer')
 
